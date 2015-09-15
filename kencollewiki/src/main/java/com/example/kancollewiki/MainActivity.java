ca9b3062fe5515dca3f1a7684492918d7404d1a6
Damon Sun <sunpeng1995@gmail.com>
2015-09-15 22:15:01 +0800

    main activity

--- kencollewiki/src/main/java/com/example/kancollewiki/MainActivity.java
+++ kencollewiki/src/main/java/com/example/kancollewiki/MainActivity.java
@@ -1,37 +1,91 @@
 package com.example.kancollewiki;
 
 import android.app.Activity;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
 
-public class MainActivity extends Activity {
+import com.zzt.inbox.interfaces.OnDragStateChangeListener;
+import com.zzt.inbox.widget.InboxLayoutBase;
+import com.zzt.inbox.widget.InboxLayoutListView;
+import com.zzt.inbox.widget.InboxBackgroundScrollView;
 
+import com.example.kancollewiki.R;
+
+
+public class MainActivity extends AppCompatActivity {
+    InboxLayoutListView inboxLayoutListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xdd000000));
+
+        final InboxBackgroundScrollView inboxBackgroundScrollView = (InboxBackgroundScrollView)findViewById(R.id.scroll);
+        inboxLayoutListView = (InboxLayoutListView)findViewById(R.id.inboxlayout);
+        inboxLayoutListView.setBackgroundScrollView(inboxBackgroundScrollView);
+        inboxLayoutListView.setCloseDistance(50);
+        inboxLayoutListView.setOnDragStateChangeListener(new OnDragStateChangeListener() {
+            @Override
+            public void dragStateChange(InboxLayoutBase.DragState state) {
+                switch (state) {
+                    case CANCLOSE:
+                        //getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xff5e5e5e));
+                        //getSupportActionBar().setTitle("back");
+                        break;
+                    case CANNOTCLOSE:
+                        //getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xdd000000));
+                        //getSupportActionBar().setTitle("InboxLayout");
+                        break;
+                }
+            }
+        });
+
+        //展开后的数据显示
+        inboxLayoutListView.setAdapter(new BaseAdapter() {
+            @Override
+            public int getCount() { return 20; }
+
+            @Override
+            public Object getItem(int position) { return null; }
+
+            @Override
+            public long getItemId(int position) { return position;}
+
+            @Override
+            public  View getView(int position, View convertView, ViewGroup parent) {
+                LayoutInflater inflater = MainActivity.this.getLayoutInflater();
+                View view = inflater.inflate(R.layout.item, null);
+                return view;
+            }
+        });
+
+        init();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void init(){
+        final LinearLayout dd = (LinearLayout)findViewById(R.id.dd);
+        dd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inboxLayoutListView.openWithAnim(dd);
+            }
+        });
+
+        final LinearLayout bb = (LinearLayout)findViewById(R.id.bb);
+        bb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inboxLayoutListView.openWithAnim(bb);
+            }
+        });
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
